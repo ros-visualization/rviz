b680de4d6d30008c0d5286fae9172a037c144fa1
Josh Faust <jfaustwg@willowgarage.com>
2009-08-08 06:00:31 +0000

    PoseWithCovarianceStamped::data -> PoseWithCovarianceStamped::pose
    git-svn-id: https://code.ros.org/svn/ros-pkg/pkg/trunk/stacks/visualization@21151 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- src/rviz/tools/pose_tool.cpp
+++ src/rviz/tools/pose_tool.cpp
@@ -160,13 +160,13 @@ int PoseTool::processMouseEvent( ViewportMouseEvent& event )
       else
       {
         geometry_msgs::PoseWithCovarianceStamped pose;
-        pose.data.pose.position.x = robot_pos_transformed.x();
-        pose.data.pose.position.y = robot_pos_transformed.y();
+        pose.pose.pose.position.x = robot_pos_transformed.x();
+        pose.pose.pose.position.y = robot_pos_transformed.y();
         tf::quaternionTFToMsg(tf::Quaternion(angle, 0.0, 0.0),
-                              pose.data.pose.orientation);
-        pose.data.covariance[6*0+0] = 0.5 * 0.5;
-        pose.data.covariance[6*1+1] = 0.5 * 0.5;
-        pose.data.covariance[6*3+3] = M_PI/12.0 * M_PI/12.0;
+                              pose.pose.pose.orientation);
+        pose.pose.covariance[6*0+0] = 0.5 * 0.5;
+        pose.pose.covariance[6*1+1] = 0.5 * 0.5;
+        pose.pose.covariance[6*3+3] = M_PI/12.0 * M_PI/12.0;
         ROS_INFO("Setting pose: %.3f %.3f %.3f [frame=%s]", 
                  robot_pos_transformed.x(),
                  robot_pos_transformed.y(),
