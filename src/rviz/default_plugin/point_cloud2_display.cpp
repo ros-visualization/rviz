13be822f54608bc02976d42c8647846d57e8798f
Josh Faust <jfaustwg@willowgarage.com>
2010-11-18 21:22:49 +0000

    Filter out nan values from PointCloud2 messages before they hit PointCloudBase, since it's a rendering hit otherwise
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/visualization/trunk@33978 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- src/rviz/default_plugin/point_cloud2_display.cpp
+++ src/rviz/default_plugin/point_cloud2_display.cpp
@@ -28,11 +28,13 @@
  */
 
 #include "point_cloud2_display.h"
+#include "point_cloud_transformers.h"
 #include "rviz/common.h"
 #include "rviz/visualization_manager.h"
 #include "rviz/properties/property.h"
 #include "rviz/properties/property_manager.h"
 #include "rviz/frame_manager.h"
+#include "rviz/validate_floats.h"
 
 #include <ros/time.h>
 #include "ogre_tools/point_cloud.h"
@@ -103,7 +105,57 @@ void PointCloud2Display::unsubscribe()
 
 void PointCloud2Display::incomingCloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud)
 {
-  addMessage(cloud);
+  // Filter any nan values out of the cloud.  Any nan values that make it through to PointCloudBase
+  // will get their points put off in lala land, but it means they still do get processed/rendered
+  // which can be a big performance hit
+  sensor_msgs::PointCloud2Ptr filtered(new sensor_msgs::PointCloud2);
+  int32_t xi = findChannelIndex(cloud, "x");
+  int32_t yi = findChannelIndex(cloud, "y");
+  int32_t zi = findChannelIndex(cloud, "z");
+
+  if (xi == -1 || yi == -1 || zi == -1)
+  {
+    return;
+  }
+
+  const uint32_t xoff = cloud->fields[xi].offset;
+  const uint32_t yoff = cloud->fields[yi].offset;
+  const uint32_t zoff = cloud->fields[zi].offset;
+  const uint8_t type = cloud->fields[xi].datatype;
+  const uint32_t point_step = cloud->point_step;
+  const size_t point_count = cloud->width * cloud->height;
+  filtered->data.resize(cloud->data.size());
+  if (point_count == 0)
+  {
+    return;
+  }
+
+  uint32_t output_count = 0;
+  const uint8_t* ptr = &cloud->data.front();
+  for (size_t i = 0; i < point_count; ++i)
+  {
+    float x = *reinterpret_cast<const float*>(ptr + xoff);
+    float y = *reinterpret_cast<const float*>(ptr + yoff);
+    float z = *reinterpret_cast<const float*>(ptr + zoff);
+    if (validateFloats(Ogre::Vector3(x, y, z)))
+    {
+      memcpy(&filtered->data.front() + (output_count * point_step), ptr, point_step);
+      ++output_count;
+    }
+
+    ptr += point_step;
+  }
+
+  filtered->header = cloud->header;
+  filtered->fields = cloud->fields;
+  filtered->data.resize(output_count * point_step);
+  filtered->height = 1;
+  filtered->width = output_count;
+  filtered->is_bigendian = cloud->is_bigendian;
+  filtered->point_step = point_step;
+  filtered->row_step = output_count;
+
+  addMessage(filtered);
 }
 
 void PointCloud2Display::targetFrameChanged()
