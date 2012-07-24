426affdb7ccf3d0d169045ce76d2c9d14ce45eb2
David Gossow <dgossow@willowgarage.com>
2012-07-24 16:05:30 +0000

    bugfix: InteractiveMarkerDisplay now actually registers as 'InteractiveMarkers'
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/visualization/trunk@39733 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- src/rviz/default_plugin/interactive_marker_display.cpp
+++ src/rviz/default_plugin/interactive_marker_display.cpp
@@ -334,4 +334,4 @@ void InteractiveMarkerDisplay::updateShowAxes()
 } // namespace rviz
 
 #include <pluginlib/class_list_macros.h>
-PLUGINLIB_DECLARE_CLASS( rviz, InteractiveMarker, rviz::InteractiveMarkerDisplay, rviz::Display )
+PLUGINLIB_DECLARE_CLASS( rviz, InteractiveMarkers, rviz::InteractiveMarkerDisplay, rviz::Display )
