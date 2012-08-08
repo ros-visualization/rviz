48fe1bec0ef672b6424256a4f56733e6bfb0992f
David Gossow <dgossow@willowgarage.com>
2012-08-08 18:28:57 +0000

    rviz: replace the point cloud sprite fragment shader with a glsl variant
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/visualization/trunk@39857 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

---
+++ ogre_media/materials/programs/point_cloud_sprite_fp.glsl
@@ -0,0 +1,17 @@
+#version 120
+
+uniform vec4 highlight;
+
+vec4 getColor( vec4 color, vec4 highlight, float dist )
+{
+  float mult = 1.0 - 0.5 * dist;
+  color = color + color*highlight;
+  return color * mult;
+}
+
+// paints a box-shaped point sprite
+void main()
+{
+  float dist = max( abs(gl_PointCoord.x-0.5), abs(gl_PointCoord.y-0.5) );
+  gl_FragColor = getColor( gl_Color, highlight, dist );
+}
--- ogre_media/materials/programs/test-compile.sh
+++ ogre_media/materials/programs/test-compile.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-cgc -profile arbvp1 -entry pointCloudPointVP_main < point_cloud_point_vp.cg
+#cgc -profile arbvp1 -entry pointCloudPointVP_main < point_cloud_point_vp.cg
+
+#cgc depth.cg -profile glslf -entry depthFP_main > depth_fp.glsl
+#cgc depth.cg -profile glslv -entry depthVP_main > depth_vp.glsl
+
+#cgc -profile glslv -entry pointCloudSpriteVP_main point_cloud_sprite_vp.cg > point_cloud_sprite_vp.glsl
+#cgc -profile glslv -entry pointCloudSpriteFP_main point_cloud_sprite_fp.cg > point_cloud_sprite_fp.glsl
+
