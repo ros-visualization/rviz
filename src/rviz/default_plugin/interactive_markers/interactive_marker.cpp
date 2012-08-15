ros-visualization/rviz

commit c8f2ecc115fb20f383c21320ccb9d57aa5dba3cc

rviz trunk: moved scene_node_ up into Display class.  Plugin displays should now use that.
git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/visualization/trunk@39883 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

src/rviz/default_plugin/interactive_markers/interactive_marker.cpp
@@ -69,7 +69,7 @@ InteractiveMarker::InteractiveMarker( InteractiveMarkerDisplay *owner, DisplayCo
   std::string feedback_topic = topic_ns+"/feedback";
   feedback_pub_ = nh.advertise<visualization_msgs::InteractiveMarkerFeedback>( feedback_topic, 100, false );
 
-  reference_node_ = context->getSceneManager()->getRootSceneNode()->createChildSceneNode();
+  reference_node_ = owner->getSceneNode();
 
   axes_node_ = reference_node_->createChildSceneNode();
   axes_ = new Axes( context->getSceneManager(), axes_node_, 1, 0.05 );
@@ -79,7 +79,6 @@ InteractiveMarker::~InteractiveMarker()
 {
   delete axes_;
   context_->getSceneManager()->destroySceneNode( axes_node_ );
-  context_->getSceneManager()->destroySceneNode( reference_node_ );
   owner_->deleteStatusStd(name_);
 }
 

src/rviz/CMakeLists.txt
@@ -83,6 +83,7 @@ qt4_wrap_cpp(MOC_FILES
 )
 
 rosbuild_add_library( ${PROJECT_NAME}
+  bit_allocator.cpp
   display.cpp
   display.cpp
   display_factory.cpp

src/rviz/bit_allocator.cpp
@@ -26,42 +26,35 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef MOCK_CONTEXT_H
-#define MOCK_CONTEXT_H
 
-#include <rviz/display_context.h>
+#include "rviz/bit_allocator.h"
 
 namespace rviz
 {
 
-class MockContext: public DisplayContext
-{
-public:
-  MockContext();
+BitAllocator::BitAllocator()
+  : allocated_bits_( 0 )
+{}
 
-  virtual Ogre::SceneManager* getSceneManager() const { return 0; }
-  virtual WindowManagerInterface* getWindowManager() const { return 0; }
-  virtual SelectionManager* getSelectionManager() const { return 0; }
-  virtual FrameManager* getFrameManager() const { return 0; }
-  virtual tf::TransformListener* getTFClient() const { return 0; }
-  virtual void queueRender() {}
-  virtual QString getFixedFrame() const { return ""; }
-  virtual uint64_t getFrameCount() const { return 0; }
-  virtual DisplayFactory* getDisplayFactory() const { return display_factory_; }
-  virtual ros::CallbackQueueInterface* getUpdateQueue() { return 0; }
-  virtual ros::CallbackQueueInterface* getThreadedQueue() { return 0; }
-  virtual void handleChar( QKeyEvent* event, RenderPanel* panel ) {};
-  virtual void handleMouseEvent( const ViewportMouseEvent& event ) {};
-  virtual ToolManager* getToolManager() const { return 0; }
-  virtual ViewManager* getViewManager() const { return 0; }
-  virtual RenderPanel* getRenderPanel() const { return 0; }
-  virtual DisplayGroup* getRootDisplayGroup() const { return 0; }
-  virtual uint32_t getDefaultVisibilityBit() const { return 0; }
-  virtual BitAllocator* visibilityBits() { return 0; }
-private:
-  DisplayFactory* display_factory_;
-};
+uint32_t BitAllocator::allocBit()
+{
+  uint32_t mask = 1;
+  for( int i = 0; i < 32; i++ )
+  {
+    if( (mask & allocated_bits_) == 0 )
+    {
+      allocated_bits_ |= mask;
+      return mask;
+    }
+
+    mask <<= 1;
+  }
+  return 0;
+}
+
+void BitAllocator::freeBits( uint32_t bits )
+{
+  allocated_bits_ &= ~bits;
+}
 
 } // end namespace rviz
-
-#endif // MOCK_CONTEXT_H

src/rviz/bit_allocator.h
@@ -26,42 +26,32 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef MOCK_CONTEXT_H
-#define MOCK_CONTEXT_H
+#ifndef BIT_ALLOCATOR_H
+#define BIT_ALLOCATOR_H
 
-#include <rviz/display_context.h>
+#include <stdint.h>
 
 namespace rviz
 {
 
-class MockContext: public DisplayContext
+/** @brief Allocation manager for bit positions within a 32-bit word. */
+class BitAllocator
 {
 public:
-  MockContext();
+  /** @brief Constructor.  All bits are free initially. */
+  BitAllocator();
 
-  virtual Ogre::SceneManager* getSceneManager() const { return 0; }
-  virtual WindowManagerInterface* getWindowManager() const { return 0; }
-  virtual SelectionManager* getSelectionManager() const { return 0; }
-  virtual FrameManager* getFrameManager() const { return 0; }
-  virtual tf::TransformListener* getTFClient() const { return 0; }
-  virtual void queueRender() {}
-  virtual QString getFixedFrame() const { return ""; }
-  virtual uint64_t getFrameCount() const { return 0; }
-  virtual DisplayFactory* getDisplayFactory() const { return display_factory_; }
-  virtual ros::CallbackQueueInterface* getUpdateQueue() { return 0; }
-  virtual ros::CallbackQueueInterface* getThreadedQueue() { return 0; }
-  virtual void handleChar( QKeyEvent* event, RenderPanel* panel ) {};
-  virtual void handleMouseEvent( const ViewportMouseEvent& event ) {};
-  virtual ToolManager* getToolManager() const { return 0; }
-  virtual ViewManager* getViewManager() const { return 0; }
-  virtual RenderPanel* getRenderPanel() const { return 0; }
-  virtual DisplayGroup* getRootDisplayGroup() const { return 0; }
-  virtual uint32_t getDefaultVisibilityBit() const { return 0; }
-  virtual BitAllocator* visibilityBits() { return 0; }
+  /** @brief Return a uint32 with a single bit "on" (previously
+   * unused), or a 0 if all bits are already allocated. */
+  uint32_t allocBit();
+
+  /** @brief Free the given bits. */
+  void freeBits( uint32_t bits );
+
 private:
-  DisplayFactory* display_factory_;
+  uint32_t allocated_bits_;
 };
 
 } // end namespace rviz
 
-#endif // MOCK_CONTEXT_H
+#endif // BIT_ALLOCATOR_H

src/rviz/config.h
@@ -26,42 +26,72 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef MOCK_CONTEXT_H
-#define MOCK_CONTEXT_H
-
-#include <rviz/display_context.h>
+#ifndef CONFIG_H
+#define CONFIG_H
 
 namespace rviz
 {
 
-class MockContext: public DisplayContext
+class ConfigWriter
 {
 public:
-  MockContext();
+  ConfigWriter();
 
-  virtual Ogre::SceneManager* getSceneManager() const { return 0; }
-  virtual WindowManagerInterface* getWindowManager() const { return 0; }
-  virtual SelectionManager* getSelectionManager() const { return 0; }
-  virtual FrameManager* getFrameManager() const { return 0; }
-  virtual tf::TransformListener* getTFClient() const { return 0; }
-  virtual void queueRender() {}
-  virtual QString getFixedFrame() const { return ""; }
-  virtual uint64_t getFrameCount() const { return 0; }
-  virtual DisplayFactory* getDisplayFactory() const { return display_factory_; }
-  virtual ros::CallbackQueueInterface* getUpdateQueue() { return 0; }
-  virtual ros::CallbackQueueInterface* getThreadedQueue() { return 0; }
-  virtual void handleChar( QKeyEvent* event, RenderPanel* panel ) {};
-  virtual void handleMouseEvent( const ViewportMouseEvent& event ) {};
-  virtual ToolManager* getToolManager() const { return 0; }
-  virtual ViewManager* getViewManager() const { return 0; }
-  virtual RenderPanel* getRenderPanel() const { return 0; }
-  virtual DisplayGroup* getRootDisplayGroup() const { return 0; }
-  virtual uint32_t getDefaultVisibilityBit() const { return 0; }
-  virtual BitAllocator* visibilityBits() { return 0; }
+  void writeToFile( const QString& file_path );
+  QString writeToString();
+
+  void keyWithInt( const QString& key_name, int value );
+  void keyWithDouble( const QString& key_name, double value );
+  void keyWithString( const QString& key_name, const QString& value );
+  void keyWithBool( const QString& key_name, bool value );
+
+  /** @brief Emits YAML::Key, key_name, YAML::Value.  Next call should be to emit a value, begin a map, or begin a sequence. */
+  void key( const QString& key_name );
+
+  void startMap();
+  void endMap();
+
+  void startSeq();
+  void endSeq();
+
+  YAML::Emitter& getEmitter();
+
 private:
-  DisplayFactory* display_factory_;
+  YAML::Emitter& emitter_;
+};
+
+class ConfigReader
+{
+public:
+  static ConfigReader* openFile( const QString& file_path );
+  static ConfigReader* openString( const QString& config_data );
+
+  bool keyWithInt( const QString& key_name, int* value_out );
+  bool keyWithDouble( const QString& key_name, double* value_out );
+  bool keyWithString( const QString& key_name, QString* value_out );
+  bool keyWithBool( const QString& key_name, bool* value_out );
+
+  ConfigReader* keyWithMap( const QString& key_name );
+  ConfigSequenceReader* keyWithSeq( const QString& key_name );
+
+  bool isValid();
+  QString printableCurrentLocation();
+  QString getFilename();
+  int getRow();
+  int getColumn();
+};
+
+class ConfigSequenceReader
+{
+public:
+  bool nextInt( int* value_out );
+  bool nextDouble( int* value_out );
+  bool nextString( int* value_out );
+  bool nextBool( int* value_out );
+  ConfigSequenceReader* nextSeq();
+  ConfigReader* nextMap();
 };
 
 } // end namespace rviz
 
-#endif // MOCK_CONTEXT_H
+#endif // CONFIG_H

src/rviz/default_plugin/camera_display.cpp
@@ -36,7 +36,6 @@
 #include <OGRE/OgreRectangle2D.h>
 #include <OGRE/OgreRenderSystem.h>
 #include <OGRE/OgreRenderWindow.h>
-#include <OGRE/OgreRoot.h>
 #include <OGRE/OgreSceneManager.h>
 #include <OGRE/OgreSceneNode.h>
 #include <OGRE/OgreTextureManager.h>
@@ -138,8 +137,8 @@ void CameraDisplay::onInitialize()
   caminfo_tf_filter_ = new tf::MessageFilter<sensor_msgs::CameraInfo>( *context_->getTFClient(), fixed_frame_.toStdString(),
                                                                        queue_size_property_->getInt(), update_nh_ );
 
-  bg_scene_node_ = scene_manager_->getRootSceneNode()->createChildSceneNode();
-  fg_scene_node_ = scene_manager_->getRootSceneNode()->createChildSceneNode();
+  bg_scene_node_ = scene_node_->createChildSceneNode();
+  fg_scene_node_ = scene_node_->createChildSceneNode();
 
   {
     static int count = 0;

src/rviz/default_plugin/depth_cloud_display.cpp
@@ -119,11 +119,7 @@ DepthCloudDisplay::DepthCloudDisplay()
 
 void DepthCloudDisplay::onInitialize()
 {
-
-  pointcloud_scene_node_ = scene_manager_->getRootSceneNode()->createChildSceneNode();
-  pointcloud_scene_node_->setVisible( true );
-
-  pointcloud_common_->initialize(context_, pointcloud_scene_node_);
+  pointcloud_common_->initialize(context_, scene_node_);
 }
 
 DepthCloudDisplay::~DepthCloudDisplay()
@@ -142,13 +138,11 @@ void DepthCloudDisplay::updateQueueSize()
 
 void DepthCloudDisplay::onEnable()
 {
-  pointcloud_scene_node_->setVisible( true );
   subscribe();
 }
 
 void DepthCloudDisplay::onDisable()
 {
-  pointcloud_scene_node_->setVisible( false );
   unsubscribe();
 
   clear();

src/rviz/default_plugin/depth_cloud_display.h
@@ -143,8 +143,6 @@ protected:
 
   boost::shared_ptr<SynchronizerDepthRGB> syncDepthRGB_;
 
-  Ogre::SceneNode* pointcloud_scene_node_;
-
   IntProperty* queue_size_property_;
   u_int32_t queue_size_;
 

commit ba2dabbb28223d6b5f5659b33d39ff0f82e1bc3f

removed bug introduced in r39883: InteractiveMarker class needs its own reference scene node, otherwise it can't move.
git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/visualization/trunk@39912 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

src/rviz/default_plugin/interactive_markers/interactive_marker.cpp
@@ -69,7 +69,7 @@ InteractiveMarker::InteractiveMarker( InteractiveMarkerDisplay *owner, DisplayCo
   std::string feedback_topic = topic_ns+"/feedback";
   feedback_pub_ = nh.advertise<visualization_msgs::InteractiveMarkerFeedback>( feedback_topic, 100, false );
 
-  reference_node_ = owner->getSceneNode();
+  reference_node_ = owner->getSceneNode()->createChildSceneNode();
 
   axes_node_ = reference_node_->createChildSceneNode();
   axes_ = new Axes( context->getSceneManager(), axes_node_, 1, 0.05 );
@@ -77,8 +77,8 @@ InteractiveMarker::InteractiveMarker( InteractiveMarkerDisplay *owner, DisplayCo
 
 InteractiveMarker::~InteractiveMarker()
 {
+  reference_node_->getParentSceneNode()->removeAndDestroyChild( reference_node_->getName() );
   delete axes_;
-  context_->getSceneManager()->destroySceneNode( axes_node_ );
   owner_->deleteStatusStd(name_);
 }
 
@@ -245,6 +245,7 @@ bool InteractiveMarker::processMessage( const visualization_msgs::InteractiveMar
   {
     std::ostringstream s;
     s << "Locked to frame " << reference_frame_;
+    s << "\nPosition: " << position_.x << " " << position_.y << " " << position_.z;
     owner_->setStatusStd( StatusProperty::Ok, name_, s.str() );
   }
   else