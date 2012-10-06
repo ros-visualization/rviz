eca45aa9cbd47cd0b20f0e2ebf92b0411fe5f786
David Gossow <dgossow@willowgarage.com>
2012-10-06 11:09:32 -0700

    made docking panel collapse buttons float over central widget

--- src/rviz/visualization_frame.h
+++ src/rviz/visualization_frame.h
@@ -207,6 +207,8 @@ protected Q_SLOTS:
 
   virtual void onDockPanelVisibilityChange( bool visible );
 
+  void onGeometryChanged();
+
 protected:
   /** @brief Initialize the default config directory (~/.rviz) and set
    * up the persistent_settings_file_ and display_config_file_
