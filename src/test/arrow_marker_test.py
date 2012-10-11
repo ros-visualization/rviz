4f2002c1f4c003d21a36bea3bebc13520906eb77
Dave Hershberger <hersh@willowgarage.com>
2012-10-11 09:09:44 -0700

    moved marker_test.py script to test directory as arrow_marker_test.py, and changed color and namespace logic so it is easier to see the different marker types individually.

--- src/test/arrow_marker_test.py
+++ src/test/arrow_marker_test.py
@@ -16,15 +16,14 @@ from std_msgs.msg import ColorRGBA
 rospy.init_node('marker_test')
 marker_pub = rospy.Publisher('marker_test', Marker)
 
-def make_marker(marker_type, idnum=[0], scale=Vector3(1,1,1)):
+def make_marker(marker_type, scale, r, g, b, a):
     # make a visualization marker array for the occupancy grid
     m = Marker()
     m.action = Marker.ADD
     m.header.frame_id = '/base_link'
     m.header.stamp = rospy.Time.now()
-    m.ns = 'marker_test'
-    m.id = idnum[0]
-    idnum[0] = idnum[0] + 1
+    m.ns = 'marker_test_%d' % marker_type
+    m.id = 0
     m.type = marker_type
     m.pose.orientation.y = 0
     m.pose.orientation.w = 1
@@ -33,17 +32,20 @@ def make_marker(marker_type, idnum=[0], scale=Vector3(1,1,1)):
     m.color.g = 0.5;
     m.color.b = 0.2;
     m.color.a = 0.3;
+    m.color.r = r;
+    m.color.g = g;
+    m.color.b = b;
+    m.color.a = a;
     return m
 
-def make_arrow_points_marker(tail, tip, idnum=[0]):
+def make_arrow_points_marker(tail, tip, idnum):
     # make a visualization marker array for the occupancy grid
     m = Marker()
     m.action = Marker.ADD
     m.header.frame_id = '/base_link'
     m.header.stamp = rospy.Time.now()
     m.ns = 'points_arrows'
-    m.id = idnum[0]
-    idnum[0] = idnum[0]+1
+    m.id = idnum
     m.type = Marker.ARROW
     m.pose.orientation.y = 0
     m.pose.orientation.w = 1
@@ -60,15 +62,14 @@ def make_arrow_points_marker(tail, tip, idnum=[0]):
  
 while not rospy.is_shutdown():
     rospy.loginfo('Publishing arrow marker')
-    counter = [0]
   
-    marker_pub.publish(make_arrow_points_marker(Point(0,0,0), Point(2,2,0), counter))
-    marker_pub.publish(make_arrow_points_marker(Point(0,0,0), Point(1,-1,1), counter))
-    marker_pub.publish(make_arrow_points_marker(Point(-1,-1,-1), Point(1,-1,-1), counter))
+    marker_pub.publish(make_arrow_points_marker(Point(0,0,0), Point(2,2,0), 0))
+    marker_pub.publish(make_arrow_points_marker(Point(0,0,0), Point(1,-1,1), 1))
+    marker_pub.publish(make_arrow_points_marker(Point(-1,-1,-1), Point(1,-1,-1), 2))
     scale = Vector3(3,2,1)
-    marker_pub.publish(make_marker(Marker.SPHERE,   counter, scale))
-    marker_pub.publish(make_marker(Marker.CYLINDER, counter, scale))
-    marker_pub.publish(make_marker(Marker.CUBE,     counter, scale))
-    marker_pub.publish(make_marker(Marker.ARROW,    counter, scale))
+    marker_pub.publish(make_marker(Marker.SPHERE,   scale, 1, .5, .2, .3))
+    marker_pub.publish(make_marker(Marker.CYLINDER, scale, .5, .2, 1, .3))
+    marker_pub.publish(make_marker(Marker.CUBE,     scale, .2, 1, .5, .3))
+    marker_pub.publish(make_marker(Marker.ARROW,    scale, 1, 1, 1, .5))
     rospy.sleep(1.0)
 
