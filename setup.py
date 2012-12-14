fecad414f2520e200a4f57e552554541c4e0efe7
Dave Hershberger <hersh@willowgarage.com>
2012-12-14 14:38:07 -0800

    Updated setup.py to use generate_distutils_setup()

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from catkin_pkg.package import parse_package_for_distutils
+from catkin_pkg.python_setup import generate_distutils_setup
 
-d = parse_package_for_distutils()
-d['packages'] = ['rviz']
-d['package_dir'] = {'': 'src/python_bindings'}
-d['install_requires'] = ['roslib', 'rospkg', 'python_qt_bindings']
+d = generate_distutils_setup(
+    packages = ['rviz'],
+    package_dir = {'': 'src/python_bindings'},
+    requires = ['roslib', 'rospkg', 'python_qt_bindings']
+)
 
 setup(**d)
