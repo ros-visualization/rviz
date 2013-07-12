6ee3213790d6615f101500feac65890a2cd1d775
David Gossow <dgossow@willowgarage.com>
2013-07-12 00:37:04 -0700

    more elegant way to set enabled_flag in property.cpp

--- src/rviz/properties/property.cpp
+++ src/rviz/properties/property.cpp
@@ -273,14 +273,7 @@ Qt::ItemFlags Property::getViewFlags( int column ) const
 {
   // if the parent propery is a disabled bool property or
   // has its own enabled view flag not set, disable this property as well
-  Qt::ItemFlags enabled_flag = Qt::ItemIsEnabled;
-  if ( parent_ )
-  {
-    if( parent_->getDisableChildren() )
-    {
-      enabled_flag = 0;
-    }
-  }
+  Qt::ItemFlags enabled_flag = ( parent_ && parent_->getDisableChildren() ) ? Qt::NoItemFlags : Qt::ItemIsEnabled;
 
   if( column == 0 || is_read_only_ )
   {
