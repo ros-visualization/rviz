b01e2559c27c03c903d3c25e18c011ed675eaa01
Thomas <ThomasMoinel@users.noreply.github.com>
2017-08-17 22:54:44 +0200

    Avoid unnecessary addPoints in PointCloudCommon::update by switching setRenderMode and addPoints (#1122)

--- src/rviz/default_plugin/point_cloud_common.cpp
+++ src/rviz/default_plugin/point_cloud_common.cpp
@@ -590,8 +590,8 @@ void PointCloudCommon::update(float wall_dt, float ros_dt)
         bool per_point_alpha = findChannelIndex(cloud_info->message_, "rgba") != -1;
 
         cloud_info->cloud_.reset( new PointCloud() );
-        cloud_info->cloud_->addPoints( &(cloud_info->transformed_points_.front()), cloud_info->transformed_points_.size() );
         cloud_info->cloud_->setRenderMode( mode );
+        cloud_info->cloud_->addPoints( &(cloud_info->transformed_points_.front()), cloud_info->transformed_points_.size() );
         cloud_info->cloud_->setAlpha( alpha_property_->getFloat(), per_point_alpha);
         cloud_info->cloud_->setDimensions( size, size, size );
         cloud_info->cloud_->setAutoSize(auto_size_);
