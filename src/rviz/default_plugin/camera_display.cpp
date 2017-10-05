59465fdd3fd46aaf75bb26d1b1e43bbf92db3e13
Shin <Tom.Cernik@gmail.com>
2017-10-05 10:03:07 +0200

    taking roi in cameraInfo in consideration when using camera_display plugin

--- src/rviz/default_plugin/camera_display.cpp
+++ src/rviz/default_plugin/camera_display.cpp
@@ -490,8 +490,15 @@ bool CameraDisplay::updateCamera()
 #endif
 
   //adjust the image rectangles to fit the zoom & aspect ratio
-  bg_screen_rect_->setCorners( -1.0f*zoom_x, 1.0f*zoom_y, 1.0f*zoom_x, -1.0f*zoom_y );
-  fg_screen_rect_->setCorners( -1.0f*zoom_x, 1.0f*zoom_y, 1.0f*zoom_x, -1.0f*zoom_y );
+  //corners are computed according to roi
+
+  double y_corner_start = (-2.0*info->roi.y_offset/info->height+1.0)*zoom_y;
+  double x_corner_start = (2.0*info->roi.x_offset/info->width-1.0)*zoom_x;
+  double y_corner_end = y_corner_start - (2.0*(info->roi.height == 0 ? info->height : info->roi.height)/info->height)*zoom_y;
+  double x_corner_end = x_corner_start + (2.0*(info->roi.width == 0 ? info->width : info->roi.width)/info->width)*zoom_x;
+
+  bg_screen_rect_->setCorners( x_corner_start, y_corner_start, x_corner_end, y_corner_end);
+  fg_screen_rect_->setCorners( x_corner_start, y_corner_start, x_corner_end, y_corner_end);
 
   Ogre::AxisAlignedBox aabInf;
   aabInf.setInfinite();
