873499db63e73c990316c749a449a99e23890b03
Victor Lamoine <victor.lamoine@gmail.com>
2019-03-07 11:58:16 +0100

    PanelDockWidget: use "mid" color from palette for background

--- src/rviz/panel_dock_widget.cpp
+++ src/rviz/panel_dock_widget.cpp
@@ -27,6 +27,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <QApplication>
 #include <QChildEvent>
 #include <QHBoxLayout>
 #include <QLabel>
@@ -46,7 +47,7 @@ PanelDockWidget::PanelDockWidget( const QString& name )
   QWidget *title_bar = new QWidget(this);
 
   QPalette pal(palette());
-  pal.setColor(QPalette::Background, QColor( 200,200,200 ) );
+  pal.setColor(QPalette::Background, QApplication::palette().color( QPalette::Mid ) );
   title_bar->setAutoFillBackground(true);
   title_bar->setPalette(pal);
   title_bar->setContentsMargins(0,0,0,0);
