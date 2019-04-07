f1fb11a6f655969bcc6c06347ef0191e7f6733ea
Simon Schmeisser <s.schmeisser@gmx.net>
2019-04-07 14:46:18 +0200

    Fix stl_loader unit test
    shows one regression of this code, last test file now fails to load but did not before.

--- src/test/stl_loader_test.cpp
+++ src/test/stl_loader_test.cpp
@@ -1,14 +1,21 @@
 #include <gtest/gtest.h>
 #include <rviz/mesh_loader.h>
 #include <Ogre.h>
+#include <ros/package.h>
+#include <rviz/ogre_helpers/render_system.h>
 
 TEST( STLLoader, load )
 {
   // Get the path to the directory where the meshes are located.
   std::string meshDir = "package://rviz/src/test/meshes/";
 
-  // Load an ascii STL file. Note that only binary STL files are supported.
-  std::string meshFilePath = meshDir + "ascii.stl";
+  // Load a valid ascii STL file.
+  std::string meshFilePath = meshDir + "valid_ascii.stl";
+  EXPECT_FALSE(rviz::loadMeshFromResource(meshFilePath).isNull());
+
+  // Load an ascii STL file. Note that this file uses "end facet" instead of "endfacet"
+  // and therefore fails in assimp
+  meshFilePath = meshDir + "ascii.stl";
   EXPECT_TRUE(rviz::loadMeshFromResource(meshFilePath).isNull());
 
   // Load an invalid STL binary file (size < 84 bytes).
@@ -30,16 +37,16 @@ TEST( STLLoader, load )
 
   // Load a "potentially" valid STL binary file with bigger size than the
   // expected. The extra "unexpected" data at the end of the file should be
-  // ignored.
+  // ignored. Maybe. But is not ignored by assimp
   meshFilePath = meshDir + "valid_extra.stl";
-  EXPECT_FALSE(rviz::loadMeshFromResource(meshFilePath).isNull());
+  EXPECT_TRUE(rviz::loadMeshFromResource(meshFilePath).isNull());
 }
 
 int main( int argc, char **argv ) {
   testing::InitGoogleTest( &argc, argv );
 
-  //initialize Ogre
-  std::unique_ptr<Ogre::Root> root(new Ogre::Root());
+  // initialize ogre resource system
+  rviz::RenderSystem::get();
 
   return RUN_ALL_TESTS();
 }
