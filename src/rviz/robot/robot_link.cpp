ros-visualization/rviz

commit 83ac8366d63fbee5985a7815fec62a2f13fefbca

work-around for buggy urdf parser for visual materials
In urdf::Link, only the first visual's material comprises actual color values, all others only have the name.
Thus to correctly show the color of all visuals, we need to search for the first visual with given material name,
much like #1079 has done. If the urdfdom fix (https://github.com/ros/urdfdom/pull/114) is released,
this work-around can be reverted.

src/rviz/robot/robot_link.cpp
@@ -442,6 +442,17 @@ Ogre::MaterialPtr RobotLink::getMaterialForLink( const urdf::LinkConstSharedPtr&
 {
   Ogre::MaterialPtr mat = Ogre::MaterialPtr(new Ogre::Material(nullptr, "robot link material", 0, ROS_PACKAGE_NAME));
 
+  // only the first visual's material actually comprises color values, all others only have the name
+  // hence search for the first visual with given material name (better fix the bug in urdf parser)
+  if (material && !material->name.empty()) {
+    for(const auto& visual : link->visual_array)
+    {
+      if (visual->material_name == material->name) {
+        material = visual->material;
+        break;
+      }
+    }
+  }
   if (!material && link->visual && link->visual->material)
     material = link->visual->material; // fallback to visual's material
 

commit 63680cf3986767cfb659b0ec8f4da524265fe3f3

Fix opacity of Collada meshes
Fixes an issue introduced in #1294 (e906f82eb2ef04e63dea03f834ae722f144bc712):
Materials of Collada mesh models need to be cloned for each instance of a robot link.

src/rviz/robot/robot_link.cpp
@@ -632,6 +632,13 @@ void RobotLink::createEntityForGeometryElement(const urdf::LinkConstSharedPtr& l
       {
         sub->setMaterial(default_material_);
       }
+      else
+      {
+          // create a new material copy for each instance of a RobotLink
+          Ogre::MaterialPtr mat = Ogre::MaterialPtr(new Ogre::Material(nullptr, material_name, 0, ROS_PACKAGE_NAME));
+          *mat = *sub->getMaterial();
+          sub->setMaterial(mat);
+      }
       materials_[sub] = sub->getMaterial();
     }
   }