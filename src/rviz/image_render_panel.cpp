4914629c8b89886b9c33323e635fc8b6139619b7
HankWong-guangdong <82137658+HankWong-guangdong@users.noreply.github.com>
2021-04-11 09:22:09 +0800

    Create image_render_panel.cpp

---
+++ src/rviz/image_render_panel.cpp
@@ -0,0 +1,16 @@
+#include "image_render_panel.h"
+namespace  rviz {
+ImageRenderPanel::ImageRenderPanel(QWidget* parent):RenderPanel(parent) {}
+ImageRenderPanel::mouseMoveEvent(QMouseEvent* event)
+{
+  QWidget::mouseMoveEvent(event);
+}
+ImageRenderPanel::mousePressEvent(QMouseEvent* event)
+{
+  QWidget::mousePressEvent(event);
+}
+ImageRenderPanel::mouseMoveEvent(QMouseEvent* event)
+{
+  QWidget::mouseMoveEvent(event);
+}
+}
