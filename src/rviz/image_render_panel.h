21825677e0f45611fdc0fd11bd1f30d0c73a4507
HankWong-guangdong <82137658+HankWong-guangdong@users.noreply.github.com>
2021-04-12 21:11:19 +0800

    Update image_render_panel.h

--- src/rviz/image_render_panel.h
+++ src/rviz/image_render_panel.h
@@ -2,16 +2,19 @@
 #define RVIZ_IMAGE_RENDER_PANEL_H
 
 #include "rviz/render_panel.h"
-namespace rviz {
-  class ImageRenderPanel ：public RenderPanel {
-  public:
-    ImageRenderPanel(QWidget* parent = nullptr);
-    ~ImageRenderPanel() override;
-  protected:
-    void mouseMoveEvent(QMouseEvent* event) override;
-    void mousePressEvent(QMouseEvent* event) override;
-    void mouseReleaseEvent(QMouseEvent* event) override;
+namespace rviz
+{
+class ImageRenderPanel ：public RenderPanel
+{
+public:
+  ImageRenderPanel(QWidget* parent = nullptr);
+  ~ImageRenderPanel() override;
+
+protected:
+  void mouseMoveEvent(QMouseEvent* event) override;
+  void mousePressEvent(QMouseEvent* event) override;
+  void mouseReleaseEvent(QMouseEvent* event) override;
 };
-}
+} // namespace rviz
 
 #endif // RVIZ_IMAGE_RENDER_PANEL_H
